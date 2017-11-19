4f70d6fb8f4416b01a0eab22e132007f0dbcf6b0
Xuefer <xuefer@gmail.com>
2017-11-19 14:05:30 +0800

    adds recoveryotazip target
    Change-Id: If23623a58c527ac39201a5e2f4c36c22c2000aa5
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- Android.mk
+++ Android.mk
@@ -711,6 +711,42 @@ LOCAL_MODULE := librecovery_ui_vr
 
 include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+# this file is included too early, so copy required values from build/core/Makefile
+DEFAULT_KEY_CERT_PAIR := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+ifneq ($(OTA_PACKAGE_SIGNING_KEY),)
+  DEFAULT_KEY_CERT_PAIR := $(OTA_PACKAGE_SIGNING_KEY)
+endif
+
+name := TWRP$(shell grep '#define TW_MAIN_VERSION_STR' $(LOCAL_PATH)/variables.h | sed -r 's#.*"(.*)".*#\1#')
+ifeq ($(TARGET_BUILD_TYPE),debug)
+  name := $(name)_debug
+endif
+name := $(name)-$(LINEAGE_VERSION)
+RECOVERY_OTA_ZIP := $(PRODUCT_OUT)/$(name).zip
+RECOVERY_OTA_FROM_TARGET_SCRIPT := $(PRODUCT_OUT)/recovery_ota_from_target_files.py
+RECOVERY_OTA_FROM_TARGET_PATCH := $(LOCAL_PATH)/scripts/ota_from_target_files.patch
+
+$(RECOVERY_OTA_FROM_TARGET_SCRIPT): $(OTA_FROM_TARGET_SCRIPT) $(RECOVERY_OTA_FROM_TARGET_PATCH)
+	cp -a "$(OTA_FROM_TARGET_SCRIPT).py" "$@".tmp
+	patch "$@".tmp $(RECOVERY_OTA_FROM_TARGET_PATCH)
+	chmod +x "$@".tmp
+	mv "$@".tmp "$@"
+
+$(RECOVERY_OTA_ZIP): KEY_CERT_PAIR := $(DEFAULT_KEY_CERT_PAIR)
+$(RECOVERY_OTA_ZIP): $(BUILT_TARGET_FILES_PACKAGE) $(DISTTOOLS) $(RECOVERY_OTA_FROM_TARGET_SCRIPT)
+	@echo -e ${CL_YLW}"Recovery OTA:"${CL_RST}" $@"
+	MKBOOTIMG=$(MKBOOTIMG) PYTHONPATH="$$PYTHONPATH:$$(readlink -f $$(dirname $(OTA_FROM_TARGET_SCRIPT)))" \
+	  "$(RECOVERY_OTA_FROM_TARGET_SCRIPT)" \
+	  --block \
+	  -p $(HOST_OUT) \
+	  -k $(KEY_CERT_PAIR) \
+	  $(if $(OEM_OTA_CONFIG), -o $(OEM_OTA_CONFIG)) \
+	  $(BUILT_TARGET_FILES_PACKAGE) $@
+
+.PHONY: recoveryotazip
+recoveryotazip: $(RECOVERY_OTA_ZIP)
+
 commands_recovery_local_path := $(LOCAL_PATH)
 
 include \
